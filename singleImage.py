ec6b061f9d20115ad4d59a050d3eb93189d5fd13
unknown <Jake.Coltman@.ad.insidemedia.net>
2015-02-20 18:32:01 +0000

    Added edge detection and circle finding algorithm

--- singleImage.py
+++ singleImage.py
@@ -14,6 +14,18 @@ def threshold_image(image):
     
     return thresh
 
+def gradiented(image):
+    
+    laplacian = cv2.Laplacian(image,cv2.CV_64F)
+    
+    return laplacian
+
+def circle_finder(image):
+    
+    circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
+    
+    print circles
+    
 
 
 im = cv2.imread('Blueye.jpg')
@@ -26,6 +38,9 @@ smallSub = cv2.resize(subtractedImage, (0,0), fx=0.5, fy=0.5)
 
 threshed = threshold_image(smallSub)
 
-cv2.imshow("frame", threshed)
+laped = gradiented(threshed)
+
+
+cv2.imshow("frame", lapedAndThreshed)
 
 cv2.waitKey() 
